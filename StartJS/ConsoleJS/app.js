01ec82c0f05de1a021b0d806b6cb4bd5cdcb1b14
YunisLee-NET <yunislee01@mail.ru>
2023-03-26 14:50:07 +0400

    First JS

---
+++ StartJS/ConsoleJS/app.js
@@ -0,0 +1,103 @@
+// console.log("Salam JS, necesen? Men C# proqramciyam.");
+// console.log("Hello everybody. I'm 33. I lick real pussy and ass.");
+
+// var a=10;
+// console.log(a);
+// console.log(typeof a);
+
+// console.log(["Perviz","Murad"]);
+
+// console.warn("Sehifede virus askarlandi");
+// console.error("Sehife tapilmadi");
+
+// var a=38;
+// var b=46;
+// var c=a+b;
+// console.log("Cavab: "+c);
+
+// var a=null;
+// console.log(a);
+// console.log(typeof a);
+
+// var array=[1,2,3,4,5,6];
+// console.log(array);
+
+// var person={
+//     name: "Parviz",
+//     surname: "Yunisli",
+//     age: 22,
+//     city: "Baku, Qaradag, Umid",
+//     uni: "ASOIU / Comp eng"
+// }
+// console.log(person);
+// console.log(typeof person);
+
+// var hour=new Date().getHours();
+// var minute=new Date().getMinutes();
+// var second=new Date().getSeconds();
+// var millisaniye=new Date().getMilliseconds();
+// console.log(hour+":"+minute+":"+second+":"+millisaniye);
+
+
+// const newArray=[1,2,3,4];
+// newArray.push(5);
+// newArray.push(6);
+// newArray.push(4);
+// console.log(newArray);
+
+// let developer="Parviz Yunisli";
+// console.info(developer);
+
+// const manName="Parviz";
+// console.error(manName);
+
+
+
+// //string convert 1
+// let example=164;
+// let convert=example.toString();
+// console.log(convert);
+// console.log(typeof convert);
+
+// //string convert 2
+// let numune=149;
+// numune=String(numune);
+// console.log(numune);
+// console.log(typeof numune);
+
+// //string convert 3
+// let misal=195;
+// misal=misal.toString();
+// console.log(misal);
+// console.log(typeof misal);
+
+// let exampleNumber;
+// exampleNumber = Number(125);
+// console.log(exampleNumber);
+// console.log(typeof exampleNumber);
+
+// const a=Number("30")+50;
+// console.log(a);
+// console.log(typeof a);
+
+// var randoom=Math.floor(Math.random()*218+1);
+// console.log(randoom);
+
+// const isci="Perviz";
+// const ixtisas="Komp muhendislik";
+// const qrup="A6212";
+// //const a=`Ad: ${isci}\nIxtisas:${ixtisas}\nQrup:${qrup}`;
+// var html="<ul>"+
+//          "<li>"+isci+"</li>"+
+//          "<li>"+ixtisas+"</li>"+
+//          "<li>"+qrup+"</li>"+
+//          "</ul>"
+
+// document.body.innerHTML=html;
+
+var arraySort=[1,9,13,26,38];
+var arraySort=arraySort.sort(function a(x,y){
+    return x-y
+});
+console.log(arraySort);
+
---
+++ StartJS/StartingJS/app.js
@@ -0,0 +1,1 @@
+alert("JavaScript'e xos gelmisiniz!");
